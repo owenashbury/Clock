9a788d00e989b75b8947051582a81cfb58c58e2f
Wallbreaker <wallbreaker@Wallbreakers-MacBook-Air.local>
2025-04-29 20:35:25 -0400

    more work on display

--- Code.py
+++ Code.py
@@ -6,7 +6,6 @@ import displayio
 import vectorio
 from adafruit_display_text import label
 from adafruit_bitmap_font import bitmap_font
-from adafruit_qualia.graphics import Graphics, Displays
 from adafruit_qualia.peripherals import Peripherals
 
 # Screen Libs
@@ -28,9 +27,8 @@ class RealTimeClock:
 
 class App:
     def __init__(self):
-        
-        round21 = Round21()
-        screen = Round21.getScreen()
+        roundScr = Round21()
+        screen = roundScr.getScreen()
         i2c = board.STEMMA_I2C()
         peripherals = Peripherals(i2c_bus=i2c)
 
@@ -48,7 +46,7 @@ class App:
         #    if peripherals.button_down:
         #        peripherals.backlight = False
         #    time.sleep(0.1)
-        
+
         clockView = ClockView()
         clockView.draw(screen)
 
@@ -70,8 +68,8 @@ class Output(ABC):
         pass
 '''
 
-class Round21():
-    def __init__():
+class Round21:
+    def __init__(self):
         release_displays()
         init_sequence_tl021wvc02 = bytes((
             0xff, 0x05, 0x77, 0x01, 0x00, 0x00, 0x10,
@@ -136,7 +134,7 @@ class Round21():
         fb = dotclockframebuffer.DotClockFramebuffer(**tft_pins, **tft_timings)
         self.screen = FramebufferDisplay(fb, auto_refresh=False)
 
-    def getScreen():
+    def getScreen(self):
         return self.screen
 
 class ClockView():
@@ -144,7 +142,7 @@ class ClockView():
         clockView = displayio.Group()
         font = bitmap_font.load_font("/Roboto-Regular-47.pcf")
         text_label = label.Label(font, text="Hello Round Display!", color=0xFF00FF)
-        
+
         clockView.append(text_label)
         screen.root_group = clockView
 
